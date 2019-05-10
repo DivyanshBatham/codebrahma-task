890701a8b07d9be8bba284f048c0935284bceb01
Divyansh Batham <divyansh.sunita@gmail.com>
2019-05-10 22:25:28 +0530

    Updates

---
+++ precache-manifest.32010b98a6f187d039e863781405908a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d45941e43301eb7892c591e0e25dacb",
+    "url": "/codebrahma-task/index.html"
+  },
+  {
+    "revision": "34738efdc06a0e71d4d7",
+    "url": "/codebrahma-task/static/css/main.7aa135db.chunk.css"
+  },
+  {
+    "revision": "8fa8c777a61bb0db5e89",
+    "url": "/codebrahma-task/static/js/2.a9d83c36.chunk.js"
+  },
+  {
+    "revision": "34738efdc06a0e71d4d7",
+    "url": "/codebrahma-task/static/js/main.4988548d.chunk.js"
+  },
+  {
+    "revision": "91ceb292c09f1721265c",
+    "url": "/codebrahma-task/static/js/runtime~main.f73b10ae.js"
+  }
+]);
